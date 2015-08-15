4e2ee991e5d5615d5f5912c003a1f91346250703
FredrickB <bieringf@gmail.com>
2015-08-15 15:57:02 +0200

    Feed posts now include link on title to show view.
    Testdata within the controller during development.

--- src/main/js/feed/feeds_controller.js
+++ src/main/js/feed/feeds_controller.js
@@ -12,10 +12,17 @@ angular.module("action").controller("FeedsController", ["$scope", "FeedsService"
 
     $scope.feeds = [
         {
+            id: 1,
             title: "Arne",
             text: "#Snippet here " +
             "##Hei",
-            snippet: "Omg what a lovely snippet",
+            snippet: "Omg what a lovely snippetdddddd Omg what a lovely snippetdddddd" +
+            "Omg what a lovely snippetddddddOmg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
+            "Omg what a lovely snippetddddddOmg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
+            "Omg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
+            "Omg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
+            "Omg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
+            "Omg what a lovely snippetddddddOmg what a lovely snippetddddddOmg what a lovely snippetdddddd",
             image: {
                 url: "https://i.imgur.com/csgtUSw.jpg"
             }
