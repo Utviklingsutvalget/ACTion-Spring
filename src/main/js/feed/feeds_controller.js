f5ed3c58f56beab735a6adb0b6262c8709b2d97f
FredrickB <bieringf@gmail.com>
2015-08-15 20:32:19 +0200

    Removed test code

--- src/main/js/feed/feeds_controller.js
+++ src/main/js/feed/feeds_controller.js
@@ -1,7 +1,5 @@
 angular.module("action").controller("FeedsController", ["$scope", "FeedsService", function($scope, FeedsService){
 
-    console.log("Hello world");
-
     $scope.feeds = function(){
         FeedsService.all().when(function(feeds){
             return feeds;
@@ -9,23 +7,4 @@ angular.module("action").controller("FeedsController", ["$scope", "FeedsService"
             return;
         });
     };
-
-    $scope.feeds = [
-        {
-            id: 1,
-            title: "Arne",
-            text: "#Snippet here " +
-            "##Hei",
-            snippet: "Omg what a lovely snippetdddddd Omg what a lovely snippetdddddd" +
-            "Omg what a lovely snippetddddddOmg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
-            "Omg what a lovely snippetddddddOmg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
-            "Omg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
-            "Omg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
-            "Omg what a lovely snippetddddddOmg what a lovely snippetdddddd" +
-            "Omg what a lovely snippetddddddOmg what a lovely snippetddddddOmg what a lovely snippetdddddd",
-            image: {
-                url: "https://i.imgur.com/csgtUSw.jpg"
-            }
-        }
-    ];
 }]);
