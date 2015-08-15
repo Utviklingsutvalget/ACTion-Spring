b7342ddbfdec02cdd729a1cdcdf917beef4acfbc
Eivind Vegsundvåg <eivindveg@hotmail.com>
2015-08-16 01:27:55 +0200

    return developmentconfig to embedded

--- src/main/java/no/swact/action/configuration/DevelopmentConfiguration.java
+++ src/main/java/no/swact/action/configuration/DevelopmentConfiguration.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
@@ -20,13 +21,7 @@ public class DevelopmentConfiguration {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
-        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
-        dataSource.setPassword(environment.getProperty("spring.datasource.pass"));
-
-        return dataSource;
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
     }
 
 }
